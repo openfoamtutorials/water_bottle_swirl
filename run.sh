f253eb50e2a8a130d630358162b9fd134606aa8e
Robert Lee <rlee32@gatech.edu>
2018-11-08 01:46:17 -0500

    updated run script for initial velocity

--- run.sh
+++ run.sh
@@ -9,9 +9,12 @@ gmsh -3 -o main.msh mesh/main.geo
 gmshToFoam main.msh -case case
 # Adjust polyMesh/boundary:
 changeDictionary -case case
-# Let's set the initial conditions (water phase)
-# First, let's load original alpha.water file.
+# Set the initial conditions for the water phase.
+# Copy original alpha.water file:
 cp case/0/alpha.water.original case/0/alpha.water
+# Copy original U file:
+cp case/0/U.original case/0/U
+# case/system/setFieldsDict specifies the initial conditions:
 setFields -case case
 # Finally, run the simulation:
 interFoam -case case
